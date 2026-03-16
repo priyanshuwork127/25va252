f0f53f49a5c60a2db03652b4ad537cf27efe122f
Priyanshu <priyanshuwork127@gmail.com>
2026-03-16 11:42:10 +0530

    DOM

---
+++ JS/dom.js
@@ -0,0 +1,12 @@
+function result(){
+    let inputValue=document.getElementById('inputData').value;
+    let displayHeading=document.getElementById('text');
+    displayHeading.innerHTML=inputValue;
+    displayHeading.style.color="red";
+    document.getElements
+}
+let paraBkg=document.getElementById('para');
+function colorChange(){
+    paraBkg.style.backgroundColor="blue";
+}
+paraBkg.addEventListener('mouseover',colorChange);
